c87c27dc17ede4773ef2f7716aa67b17820b072c
rockintuna <rockintuna@naver.com>
2025-03-30 22:05:39 +0900

    feat: Memo HTTP Request function

--- yobeondal/HTTPClient.swift
+++ yobeondal/HTTPClient.swift
@@ -99,4 +99,47 @@ class HTTPClient {
             
         }.resume()
     }
+    
+    func getMemos(_ year: Int,_ month: Int, completion: @escaping (Result<[Memo], NetworkError>) -> Void) {
+        guard let url = URL.getMemosUrl(year, month) else {
+            return completion(.failure(.badURL))
+        }
+
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            guard let data = data, error == nil else {
+                return completion(.failure(.noData))
+            }
+
+            guard let memos = try? JSONDecoder().decode([Memo].self, from: data) else {
+                return completion(.failure(.decodingError))
+            }
+
+            completion(.success(memos))
+            
+        }.resume()
+    }
+    
+    func createMemo(_ year: Int,_ month: Int,_ content: String,_ userId: Int, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        
+        guard let url = URL.postMemosUrl() else {
+            return completion(.failure(.badURL))
+        }
+        
+        let memoData: [String: Any] = ["content" : content, "userId" : userId, "year" : year, "month" : month]
+        guard let jsonData = try? JSONSerialization.data(withJSONObject: memoData) else { return }
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = jsonData
+    
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                return completion(.failure(.noData))
+            }
+            
+            completion(.success("OK"))
+            
+        }.resume()
+    }
 }
---
+++ yobeondal/MemoViewModel.swift
@@ -0,0 +1,50 @@
+//
+//  Untitled.swift
+//  yobeondal
+//
+//  Created by 이정인 on 3/11/25.
+//
+
+import Foundation
+import SwiftUI
+
+class MemoViewModel: ObservableObject {
+    
+    @Published var response: [Memo] = []
+    @Published var success: Bool? = nil
+    var httpClient = HTTPClient()
+    
+    
+    func getMemos(_ year: Int,_ month: Int) {
+        httpClient.getMemos(year, month) { result in
+            DispatchQueue.main.async { // ✅ 모든 결과 처리를 메인 스레드에서 실행
+                switch result {
+                case .success(let results):
+                    print("✅ HTTP GET MEMO SUCCESS")
+                    self.response = results
+                    
+                case .failure(let error):
+                    print("❌ Error: \(error.localizedDescription)")
+                    self.response = [] // 🚨 실패 시 UI 업데이트가 필요하다면 이렇게 할 수도 있음
+                }
+            }
+        }
+    }
+    
+    func createMemo(_ tid: Int?, _ year: Int,_ month: Int,_ content: String,_ userId: Int) {
+        httpClient.createMemo(year, month, content, userId) { result in
+            DispatchQueue.main.async { // ✅ 모든 결과 처리를 메인 스레드에서 실행
+                switch result {
+                case .success(let results):
+                    print("✅ Memo create SUCCESS")
+                    self.success = true
+                    self.getMemos(year, month)
+                    
+                case .failure(let error):
+                    print("❌ Error: \(error.localizedDescription)")
+                    self.success = false
+                }
+            }
+        }
+    }
+}
